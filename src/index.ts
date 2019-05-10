1d0cc260a699b3061fe2238008d2d4b8b94e51d0
Quinn Blenkinsop <quinn@qw-in.com>
2019-05-10 13:33:16 -0700

    Add linting

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+export interface Crisp {
+  /**
+   * Installed version of Crisp
+   */
+  Version: string;
+}
+
+const Version = 'TODO';
+
+export { Version };
+
+const Crisp: Crisp = {
+  Version
+};
+
+export default Crisp;
